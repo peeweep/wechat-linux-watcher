35522812a92c123009a670ed8fc334de0e27cd59
jinqiang zhang <jinqiang@zhang.my>
2024-12-26 12:12:26 +0000

    update

--- wechat-linux-version.sh
+++ wechat-linux-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb -O /tmp/wechat-amd64.deb
-if [ -f /tmp/wechat-amd64.deb ];then
-	dpkg --info /tmp/wechat-amd64.deb  | grep Version | awk  '{print $NF}' >> wechat-linux-version.txt
+if [ -f /tmp/wechat-amd64.deb ]; then
+  dpkg --info /tmp/wechat-amd64.deb | grep Version | awk '{print $NF}' >wechat-linux-version.txt
 fi
